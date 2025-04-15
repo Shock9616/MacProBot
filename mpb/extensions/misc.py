7157ea8467666020d5dea8ea8c845076a00a7884
Shock9616 <Shock9616@users.noreply.github.com>
2025-04-14 22:19:11 -0700

    Revert last commit

--- mpb/extensions/misc.py
+++ mpb/extensions/misc.py
@@ -87,7 +87,7 @@ class EnvironmentVariablesAlias(
     @lb.invoke
     async def invoke(self, ctx: lb.Context):
         _ = await ctx.respond(
-            "# Environment Variables\n"
+            "# Environment Variables\n\n"
             + "## [Metal HUD ↗](https://discord.com/channels/1235385881586831451/1237550504846823425/1242331906146570351)\n"
             + "Commands to show or hide the Metal HUD (heads-up display for Apple's Metal graphics API), which displays performance metrics.\n"
             + "### HUD ON\n"
@@ -96,7 +96,7 @@ class EnvironmentVariablesAlias(
             + "### HUD OFF\n"
             + "Hides Metal HUD permanently by default\n"
             + "```/bin/launchctl setenv MTL_HUD_ENABLED 0```\n"
-            + "## Allow AVX/FC16\n"
+            + "## Allow AVK/FC16\n"
             + "### Enable AVX\n"
             + "A command to enable Advanced Vector Extensions in CrossOver (likely for improved performance or get past game crashing).\n"
             + "```ROSETTA_ADVERTISE_AVX=1```\n"
@@ -104,10 +104,10 @@ class EnvironmentVariablesAlias(
             + "### MetalFX\n"
             + "**Enable Spatial Upscaling**\n"
             + "Activates MetalFX's spatial upscaling feature, which upscales the rendered image spatially to a higher resolution, typically doubling the output resolution by default.\n"
-            + '```"DXMT_METALFX_SPATIAL_SWAPCHAIN" = "1"```\n'
+            + '```"DXMT_METALFX_SPATIAL_SWAPCHAIN" = "1"```\n\n'
             + "**Set Upscaling Factor to 2**\n"
             + "Configures scaling to a factor of 2.0 = the rendered image will be upscaled by a factor of 2 in both width and height, effectively quadrupling the pixel count of the output compared to the internal render resolution\n"
-            + '```"d3d11.metalSpatialUpscaleFactor" = "2.0"```\n'
+            + '```"d3d11.metalSpatialUpscaleFactor" = "2.0"```\n\n'
             + "**Cap Frame Rate at 60**\n"
             + "Instructs DXMT to limit the maximum frame rate to 60 frames per second\n"
             + '```"DXMT_CONFIG=d3d11.preferredMaxFrameRate" = "60"```\n'
