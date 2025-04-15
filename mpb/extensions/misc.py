13419531177216e357098dd9fd27efe77dbbd19f
Shock9616 <Shock9616@users.noreply.github.com>
2025-04-14 19:54:45 -0700

    Add environmentvariables command and ev alias

--- mpb/extensions/misc.py
+++ mpb/extensions/misc.py
@@ -37,3 +37,54 @@ class UpdateDxmt(
     @lb.invoke
     async def invoke(self, ctx: lb.Context):
         _ = await ctx.respond("https://www.youtube.com/watch?v=5uIEd-6DqFM")
+
+
+@loader.command
+class EnvironmentVariables(
+    lb.SlashCommand,
+    name="environmentvariables",
+    aliases=["ev"],
+    description="Reply with some useful environment variables for Mac gaming",
+):
+    @lb.invoke
+    async def invoke(self, ctx: lb.Context):
+        _ = await ctx.respond(
+            "**Metal HUD**\n\n"
+            + "__HUD ON__\n"
+            + "/bin/launchctl setenv MTL_HUD_ENABLED 1\n\n"
+            + "__HUD OFF__\n"
+            + "/bin/launchctl setenv MTL_HUD_ENABLED 0\n\n"
+            + "**Allow AVK/FC16**\n\n"
+            + "ROSETTA_ADVERTISE_AVX=1\n\n"
+            + "**DXMT**\n\n"
+            + "__MetalFX__\n"
+            + '"DXMT_METALFX_SPATIAL_SWAPCHAIN" = "1"\n'
+            + '"d3d11.metalSpatialUpscaleFactor" = "2.0"\n\n'
+            + "__Frame Rate Cap__\n"
+            + '"DXMT_CONFIG=d3d11.preferredMaxFrameRate" = "60"'
+        )
+
+
+@loader.command
+class EnvironmentVariablesAlias(
+    lb.SlashCommand,
+    name="ev",
+    description="Reply with some useful environment variables for Mac gaming",
+):
+    @lb.invoke
+    async def invoke(self, ctx: lb.Context):
+        _ = await ctx.respond(
+            "**Metal HUD**\n\n"
+            + "__HUD ON__\n"
+            + "/bin/launchctl setenv MTL_HUD_ENABLED 1\n\n"
+            + "__HUD OFF__\n"
+            + "/bin/launchctl setenv MTL_HUD_ENABLED 0\n\n"
+            + "**Allow AVK/FC16**\n\n"
+            + "ROSETTA_ADVERTISE_AVX=1\n\n"
+            + "**DXMT**\n\n"
+            + "__MetalFX__\n"
+            + '"DXMT_METALFX_SPATIAL_SWAPCHAIN" = "1"\n'
+            + '"d3d11.metalSpatialUpscaleFactor" = "2.0"\n\n'
+            + "__Frame Rate Cap__\n"
+            + '"DXMT_CONFIG=d3d11.preferredMaxFrameRate" = "60"'
+        )
