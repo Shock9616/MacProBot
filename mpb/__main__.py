9cad5ced6d17ac1b69db46fa5c6898f526a27df7
Shock9616 <Shock9616@users.noreply.github.com>
2025-05-22 21:17:55 -0700

    Re-enable AI features with a different model

--- mpb/__main__.py
+++ mpb/__main__.py
@@ -11,9 +11,10 @@ from collections.abc import Sequence
 import dotenv
 import hikari as hk
 import lightbulb as lb
+from openai import OpenAI
 
 from . import extensions
-from .constants import sassy_responses
+from .constants import ai_prompt, sassy_responses
 
 _ = dotenv.load_dotenv()
 
@@ -51,38 +52,39 @@ async def on_bot_mentioned(event: hk.MessageCreateEvent):
         _ = await event.message.respond(random.choice(sassy_responses))
 
 
-# @bot.listen(hk.MessageCreateEvent)
-# async def on_message_created(event: hk.MessageCreateEvent):
-#     """Occasionally use AI to respond to a message unprompted"""
-#
-#     if not event.message.content or event.is_bot:  # Ignore empty messages and bots
-#         return
-#
-#     if len(event.message.content.split()) > 8 and random.randint(0, 100) == 69:
-#         # Respond to ~1/100 messages that are more than 8 words long
-#
-#         client = OpenAI(
-#             base_url="https://openrouter.ai/api/v1",
-#             api_key=os.environ["AI_API_KEY"],
-#         )
-#
-#         completion = client.chat.completions.create(
-#             extra_headers={},
-#             extra_body={},
-#             model="deepseek/deepseek-r1:free",
-#             messages=[
-#                 {"role": "developer", "content": ai_prompt},
-#                 {
-#                     "role": "user",
-#                     "content": event.message.content,
-#                 },
-#             ],
-#         )
-#
-#         response = completion.choices[0].message.content
-#
-#         if response is not None:
-#             _ = await event.message.respond(response.strip('"'))
+@bot.listen(hk.MessageCreateEvent)
+async def on_message_created(event: hk.MessageCreateEvent):
+    """Occasionally use AI to respond to a message unprompted"""
+
+    if not event.message.content or event.is_bot:  # Ignore empty messages and bots
+        return
+
+    # if len(event.message.content.split()) > 8 and random.randint(0, 100) == 69:
+    if len(event.message.content.split()) > 8:
+        # Respond to ~1/100 messages that are more than 8 words long
+
+        client = OpenAI(
+            base_url="https://openrouter.ai/api/v1",
+            api_key=os.environ["AI_API_KEY"],
+        )
+
+        completion = client.chat.completions.create(
+            extra_headers={},
+            extra_body={},
+            model="meta-llama/llama-3.3-70b-instruct:free",
+            messages=[
+                {"role": "developer", "content": ai_prompt},
+                {
+                    "role": "user",
+                    "content": event.message.content,
+                },
+            ],
+        )
+
+        response = completion.choices[0].message.content
+
+        if response is not None:
+            _ = await event.message.respond(response.strip('"'))
 
 
 if __name__ == "__main__":
