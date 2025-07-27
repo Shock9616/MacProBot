e1cf8aec19d182b2db5419796263306ff55e3eae
Shock9616 <Shock9616@users.noreply.github.com>
2025-07-26 23:29:59 -0700

    More sassy responses

--- mpb/constants.py
+++ mpb/constants.py
@@ -55,6 +55,12 @@ sassy_responses = [
     "Ah, a Mac gamer. Admiring your commitment to playing _checks notes_ four whole games at 15 fps. Truly living on the edge.",
     "Please refer to our #rules and channel topics and then head to #support",
     "imagine that you have 0 cookies and you split them evenly among 0 friends, how many cookies does each person get? see? it doesn't make sense. and cookie monster is sad that there are no cookies. and _you_ are sad that you have no friends.",
+    "we both said a lot of things that you're going to regret",
+    "hello this is the part where i kill you",
+    "we do what we must because we can",
+    "you, [subject name here], must be the pride of [subject hometown here]",
+    "it's not out of the question that you might have a very minor case of serious brain damage",
+    "aw. bless your little primate brain",
 ]
 
 glossary = {
