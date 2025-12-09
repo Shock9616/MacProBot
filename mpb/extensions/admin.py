b22a2e74b7e8e87cfb75ad7868ac5bbbcb9ab088
Shock9616 <Shock9616@users.noreply.github.com>
2025-12-08 17:49:03 -0800

    Add /choosewinner command for giveaways

--- mpb/extensions/admin.py
+++ mpb/extensions/admin.py
@@ -5,6 +5,8 @@
 #
 
 import os
+import random
+import re
 
 import dotenv
 import hikari as hk
@@ -13,6 +15,11 @@ import lightbulb as lb
 _ = dotenv.load_dotenv()
 
 loader = lb.Loader()
+
+
+MESSAGE_LINK_RE = re.compile(
+    r"https://(?:canary\.|ptb\.)?discord\.com/channels/\d+/(\d+)/(\d+)"
+)
 
 
 async def is_mod(member: hk.Member) -> bool:
@@ -67,3 +74,48 @@ class Announce(
             announcements_channel, content.decode("utf-8")
         )
         _ = await ctx.respond("Announcement sent!")
+
+
+@loader.command
+class ChooseWinner(
+    lb.SlashCommand,
+    name="choosewinner",
+    description="Select a random user who reacted to a message",
+):
+    # Options
+    link: str = lb.string("message", "The message to choose a user from")
+
+    @lb.invoke
+    async def invoke(self, ctx: lb.Context):
+        # Check if calling user is a mod
+        if ctx.member is not None and not await is_mod(ctx.member):
+            print(await ctx.member.fetch_roles())
+            _ = await ctx.respond(
+                "Sorry, you don't have permission to execute this command",
+                ephemeral=True,
+            )
+            return
+
+        match = MESSAGE_LINK_RE.match(self.link)
+        if not match:
+            _ = await ctx.respond(
+                "Sorry, the provided link is invalid. Please try again with a valid link",
+                ephemeral=True,
+            )
+            return
+
+        channel_id = int(match.group(1))
+        message_id = int(match.group(2))
+
+        reactions = [
+            r
+            async for r in ctx.client.rest.fetch_reactions_for_emoji(
+                channel_id, message_id, "🍎"
+            )
+        ]
+
+        winner = random.choice(reactions)
+
+        _ = await ctx.respond(
+            f"Congratulations to {winner.mention} on winning the giveaway!"
+        )
